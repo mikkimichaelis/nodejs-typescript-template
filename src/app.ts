51f895e73f65decaeab532ec7b7fc0d1b681ae39
Mikki Michaelis <mikki@psychward.software>
2023-07-11 11:23:16 -0500

    wip

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,12 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const sum = (a: number, b: number): number => {
-  return a + b;
-};
+
+const browserObject = require('./browser');
+const scraperController = require('./pageController');
+
+//Start the browser and create a browser instance
+let browserInstance = browserObject.startBrowser();
+
+// Pass the browser instance to the scraper controller
+scraperController(browserInstance)
---
+++ src/browser.ts
@@ -0,0 +1,17 @@
+// const puppeteer = require('puppeteer');
+import * as puppeteer from 'puppeteer';
+
+export async function startBrowser(){
+	let browser;
+	try {
+	    console.log("Opening the browser......");
+	    browser = await puppeteer.launch({
+	        headless: false,
+	        args: ["--disable-setuid-sandbox"],
+	        'ignoreHTTPSErrors': true
+	    });
+	} catch (err) {
+	    console.log("Could not create a browser instance => : ", err);
+	}
+	return browser;
+}
